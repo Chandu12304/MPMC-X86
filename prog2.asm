d507e1dd7090c1dc28919afefa851835ca8ffc75
Chandan U <chandanucodeid04@gmail.com>
2024-10-07 20:55:02 +0530

    Update prog2.asm

--- prog2.asm
+++ prog2.asm
@@ -2,9 +2,9 @@
 .STACK 100H
 
 .DATA
-    ; Array of numbers to sort
-    NUMS DW 5, 3, 8, 1, 6  ; Array elements
-    COUNT DW 5              ; Number of elements in the array
+    ; Array of numbers to sort (using DB)
+    NUMS DB 5, 3, 8, 1, 6  ; Array elements (as bytes)
+    COUNT DB 5              ; Number of elements in the array (as a byte)
     MSG DB 'Sorting complete$', 0  ; Message to display after sorting
 
 .CODE
@@ -22,16 +22,16 @@ OUTER_LOOP:
     MOV BX, CX           ; BX holds the number of comparisons in each pass
     
 INNER_LOOP:
-    MOV AX, NUMS[SI]     ; Load current element into AX
-    CMP AX, NUMS[SI+2]   ; Compare it with the next element
+    MOV AL, NUMS[SI]     ; Load current element into AL (byte register)
+    CMP AL, NUMS[SI+1]   ; Compare it with the next element
     JBE NO_SWAP          ; If current element <= next element, no swap needed
     
     ; Swap the elements if current element > next element
-    XCHG AX, NUMS[SI+2]  ; Swap AX with the next element
-    MOV NUMS[SI], AX     ; Store the swapped value back in current position
+    XCHG AL, NUMS[SI+1]  ; Swap AL with the next element
+    MOV NUMS[SI], AL     ; Store the swapped value back in current position
     
 NO_SWAP:
-    ADD SI, 2            ; Move to the next pair of elements (each DW is 2 bytes)
+    INC SI               ; Move to the next pair of elements (each DB is 1 byte)
     DEC BX               ; Decrease comparison count
     JNZ INNER_LOOP       ; If BX != 0, repeat comparisons
     
