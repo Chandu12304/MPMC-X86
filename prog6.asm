c39c74571f3174070bb21101fa3f14ad9f3f7b60
Chandan U <chandanucodeid04@gmail.com>
2024-10-08 10:38:29 +0530

    Update prog6.asm

--- prog6.asm
+++ prog6.asm
@@ -1,3 +1,4 @@
+;6a
 AREA    PROG1, CODE, READONLY   ; Define a code area
         ENTRY                           ; Mark the entry point of the program
 
@@ -19,5 +20,44 @@ STOP
         DCD     0X10, 0X20, 0X30, 0X40, 0X50  ; Data to be copied
 
         AREA    DEST, DATA, READWRITE    ; Define a data area for DEST (empty)
+
+        END                             ; End of the program
+
+;6b
+AREA    PROG2, CODE, READONLY   ; Define a code area (PROG2)
+        ENTRY                           ; Mark the entry point of the program
+
+START
+        LDR     R1, =0X00000006         ; Load the value 6 into R1
+        LDR     R2, =0X00000001         ; Load the value 1 into R2
+
+        ADD     R5, R1, R2              ; Add R1 and R2, store result in R5 (R5 = 6 + 1 = 7)
+        SUB     R6, R1, R2              ; Subtract R2 from R1, store result in R6 (R6 = 6 - 1 = 5)
+        MUL     R7, R1, R2              ; Multiply R1 by R2, store result in R7 (R7 = 6 * 1 = 6)
+
+STOP
+        B       STOP                    ; Infinite loop to stop the program
+
+        END                             ; End of the program
+
+;6c
+AREA    PROG2, CODE, READONLY   ; Define a code area (PROG2)
+        ENTRY                           ; Mark the entry point of the program
+
+START
+        LDR     R1, =0X00000003         ; Load the value 3 into R1 (Binary: 00000011)
+        LDR     R2, =0X00000007         ; Load the value 7 into R2 (Binary: 00000111)
+
+        AND     R3, R1, R2              ; Logical AND between R1 and R2, store result in R3
+                                        ; R3 = 00000011 AND 00000111 = 00000011 (0x3)
+
+        ORR     R4, R1, R2              ; Logical OR between R1 and R2, store result in R4
+                                        ; R4 = 00000011 OR 00000111 = 00000111 (0x7)
+
+        EOR     R5, R1, R2              ; Logical XOR between R1 and R2, store result in R5
+                                        ; R5 = 00000011 XOR 00000111 = 00000100 (0x4)
+
+STOP
+        B       STOP                    ; Infinite loop to stop the program
 
         END                             ; End of the program
