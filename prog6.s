83c6c4db5ed6afc4c53ef0dd093781929c9321c3
Chandan U <chandanucodeid04@gmail.com>
2024-11-12 22:43:41 +0530

    Update prog6.s

--- prog6.s
+++ prog6.s
@@ -13,8 +13,8 @@
 	ORR R6,R0,R1 
 	EOR R7,R0,R1 
 
- LDR R9, =0x20           
- STR R9, [R0]            
+    LDR R9, =0x20           
+   STR R9, [R0]            
  LDR R10, [R0]
  ADD R12, R11, #10       
  PUSH {R0, R1, R2}       
