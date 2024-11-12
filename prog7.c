62e57de4511b3072bf742905a1f74ac0be115df6
Chandan U <chandanucodeid04@gmail.com>
2024-11-12 16:57:11 +0530

    Update prog7.c

--- prog7.c
+++ prog7.c
@@ -7,24 +7,22 @@ int sum;
 int sub;        
 int mul;        
 int div;        
-int and_result;   // Variable to store the AND result
-int or_result;    // Variable to store the OR result
-int xor_result;   // Variable to store the XOR result
-int not_result;   // Variable to store the NOT result of 'a'
+int and_result;   
+int or_result;    
+int xor_result;   
+int not_result;
 
 int main(void)
 {
-    // Arithmetic operations
     sum = a + b;    
     sub = a - b;    
     mul = a * b;    
     div = a / b;    
 
-    // Logical operations
-    and_result = a & b;  // Perform bitwise AND operation
-    or_result = a | b;   // Perform bitwise OR operation
-    xor_result = a ^ b;  // Perform bitwise XOR operation
-    not_result = ~a;     // Perform bitwise NOT operation on 'a'
+    and_result = a & b;  
+    or_result = a | b;   
+    xor_result = a ^ b;  
+    not_result = ~a;    
 
     // Infinite loop
     while (1) { }
